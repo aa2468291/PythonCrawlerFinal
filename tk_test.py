bc6338ca12378b69562fcecfd6cb8930b232b600
aa2468291 <aa2468291@gmail.com>
2020-01-02 23:51:34 +0800

    UI 更新

--- tk_test.py
+++ tk_test.py
@@ -23,7 +23,7 @@ def cleaning(raw):
 
 def coolpc():
     print('原價屋資料撈取ing......')
-    message1_label = tk.Label(work_frame, text='原價屋資料撈取ing......')
+    message1_label = tk.Label(work_frame, text='原價屋資料撈取ing......', font=("微軟正黑體", 12))
     message1_label.pack()
 
     # message1_label.configure(text='原價屋資料撈取ing......')
@@ -40,7 +40,7 @@ def coolpc():
                 cleaning(i)
 
 
-    message2_label = tk.Label(work_frame, text='原價屋資料撈取完畢!!!')
+    message2_label = tk.Label(work_frame, text='原價屋資料撈取完畢!!!', font=("微軟正黑體", 12))
     message2_label.pack()
     window.update()
 
@@ -57,7 +57,7 @@ def sinya():
     if r.status_code == requests.codes.ok:  # 確認網頁狀態
 
         print('欣亞數位資料撈取ing , 約20秒')
-        message3_label = tk.Label(work_frame, text='欣亞數位資料撈取ing , 約20秒')
+        message3_label = tk.Label(work_frame, text='欣亞數位資料撈取ing , 約20秒', font=("微軟正黑體", 12))
         message3_label.pack()
         window.update()
 
@@ -70,7 +70,7 @@ def sinya():
 
         text = '資料清單共' + str(len(pre_num)) + '個，再等我一下......'
         print(text)
-        message4_label = tk.Label(work_frame, text=text)
+        message4_label = tk.Label(work_frame, text=text, font=("微軟正黑體", 12))
 
         message4_label.pack()
         window.update()
@@ -90,13 +90,11 @@ def sinya():
                 sinyaData[i] = j
 
         # message1_label.configure(text=str(text)+',欣亞數位資料撈取完畢!!!')
-        message5_label = tk.Label(work_frame, text='欣亞數位資料撈取完畢')
+        message5_label = tk.Label(work_frame, text='欣亞數位資料撈取完畢', font=("微軟正黑體", 12))
         message5_label.pack()
         window.update()
         print('欣亞數位資料撈取完畢')
         sinya_search()
-
-
 
 
 def coolpc_search():
@@ -105,30 +103,39 @@ def coolpc_search():
     symbol = ['↪', '❤', '共有商品']
     for j in coolpcData:
         if all(x in j.lower() for x in keyword) and not any(x in j for x in symbol):
-            coolpc_listbox.insert(END, str(j)+' '+str(coolpcData[j]))
+            coolpc_listbox.insert(END, '$ '+str(coolpcData[j])+'  ///  '+str(j))
+
 
 def sinya_search():
     sinya_listbox.delete(0, 'end')
     keyword = keyword_entry.get().lower().split()
     for k in sinyaData:
         if all(x in k.lower() for x in keyword):
-            sinya_listbox.insert(END, str(k) + ' ' + str(sinyaData[k]))
-            # print(k, sinyaData[k])
+            sinya_listbox.insert(END, '$ '+str(sinyaData[k])+'  ///  '+str(k))
 
 
+def search_all(event=None):
+    coolpc_search()
+    window.update()
+    sinya_search()
+    window.update()
 
-header_label = tk.Label(window, text='PCDIY爬蟲比價系統')
+
+header_label = tk.Label(window, text='PCDIY爬蟲比價系統', font=("Helvetica", 32))
 header_label.pack()
 
 keyword_frame = tk.Frame(window)
 keyword_frame.pack(side='top')
-keyword_label = tk.Label(keyword_frame, text='請輸入要搜尋的商品，可以用空格多關鍵字搜尋')
+keyword_label = tk.Label(keyword_frame, text='請輸入要搜尋的商品，可以用空格多關鍵字搜尋', font=("微軟正黑體", 12))
 keyword_label.pack()
 
-keyword_entry = tk.Entry(keyword_frame, width='40')
+keyword_entry = tk.Entry(keyword_frame, width='40', font=("微軟正黑體", 16))
 keyword_entry.pack(side='left')
-submit_btn = tk.Button(keyword_frame, text='送出搜尋',command=lambda:[coolpc_search(), sinya_search()])
+submit_btn = tk.Button(keyword_frame, text='送出搜尋', command=lambda: [coolpc_search(), sinya_search()], font=("微軟正黑體", 12), relief='solid')
 submit_btn.pack(side='left')
+
+keyword_entry.bind("<Return>", search_all)
+
 
 list_frame = tk.Frame(window)
 list_frame.pack(side='top')
@@ -136,6 +143,10 @@ list_frame.pack(side='top')
 
 coolpc_list_frame = tk.Frame(list_frame)
 coolpc_list_frame.pack(side='left', fill='y')
+
+coolpc_title_label = tk.Label(coolpc_list_frame, text='原價屋', font=("微軟正黑體", 16), fg='blue')
+coolpc_title_label.pack()
+
 
 coolpc_scrollbar = Scrollbar(coolpc_list_frame)
 coolpc_scrollbar.pack(side='right', fill='y')
@@ -151,6 +162,10 @@ coolpc_listbox.config(yscrollcommand=coolpc_scrollbar.set)
 coolpc_scrollbar.config(command=coolpc_listbox.yview)
 sinya_list_frame = tk.Frame(list_frame)
 sinya_list_frame.pack(side='right', fill='y')
+
+sinya_title_label = tk.Label(sinya_list_frame, text='欣亞數位', font=("微軟正黑體", 16), fg='green')
+sinya_title_label.pack()
+
 sinya_scrollbar = Scrollbar(sinya_list_frame)
 sinya_scrollbar.pack(side='right', fill='y')
 
@@ -166,7 +181,7 @@ sinya_scrollbar.config(command=sinya_listbox.yview)
 
 work_frame = tk.Frame(window)
 work_frame.pack()
-work_label = tk.Label(work_frame, text='=========系統訊息==========')
+work_label = tk.Label(work_frame, text='=========系統訊息==========', font=("微軟正黑體", 24))
 work_label.pack()
 
 # message2_label = tk.Label(work_frame, text='')
@@ -176,7 +191,7 @@ work_label.pack()
 # message4_label = tk.Label(work_frame, text='')
 # message4_label.pack()
 
-get_btn = tk.Button(window, text='點我開撈', command=lambda: [coolpc(), sinya()])
+get_btn = tk.Button(window, text='點我開始爬網站資料', command=lambda: [coolpc(), sinya()], font=("微軟正黑體", 14), relief='solid', fg='red')
 get_btn.pack()
 window.mainloop()
 
