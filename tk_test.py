f8845252c62881e13fb3c950723b77474768e981
aa2468291 <aa2468291@gmail.com>
2020-01-02 22:14:10 +0800

    ++

--- tk_test.py
+++ tk_test.py
@@ -8,7 +8,6 @@ window.title('PCDIY爬蟲比價系統')
 window.geometry('1920x1080')
 window.configure()
 
-
 coolpcData = {}
 sinyaData = {}
 
@@ -17,14 +16,17 @@ def cleaning(raw):
     left = raw.rfind('$')
     left2 = raw.find('$')
     right = raw.rfind(' ')
-    money = raw[left:right].replace('◆', '').replace('★', '').replace('熱賣', '').strip()
+    money = raw[left:right].replace('◆', '').replace('★', '').replace('熱賣', '').strip()[1:]
     product = raw[:left2 - 2]
     coolpcData[product] = money
 
 
 def coolpc():
     print('原價屋資料撈取ing......')
-    message1_label.configure(text='原價屋資料撈取ing......')
+    message1_label = tk.Label(work_frame, text='原價屋資料撈取ing......')
+    message1_label.pack()
+
+    # message1_label.configure(text='原價屋資料撈取ing......')
     window.update()
     r = requests.get('http://www.coolpc.com.tw/evaluate.php')  # 爬取網頁內容
     if r.status_code == requests.codes.ok:  # 確認網頁狀態
@@ -37,24 +39,26 @@ def coolpc():
             if len(i) > 2:
                 cleaning(i)
 
-    message2_label.configure(text='原價屋資料撈取完畢!!!')
+
+    message2_label = tk.Label(work_frame, text='原價屋資料撈取完畢!!!')
+    message2_label.pack()
     window.update()
+
     coolpc_search()
 
     print('原價屋資料撈取完畢')
 
 
-
 def sinya():
     r = requests.get('https://www.sinya.com.tw/diy')
     r.encoding = 'utf-8'  # 設定編碼為UTF-8
-
     pre_num = []  # 預計要爬取的ID list
 
     if r.status_code == requests.codes.ok:  # 確認網頁狀態
 
         print('欣亞數位資料撈取ing , 約20秒')
-        message3_label.configure(text='欣亞數位資料撈取ing , 約20秒')
+        message3_label = tk.Label(work_frame, text='欣亞數位資料撈取ing , 約20秒')
+        message3_label.pack()
         window.update()
 
 
@@ -64,8 +68,13 @@ def sinya():
         for num in numbers:
             pre_num.append(num.get('data-id'))
 
-        text = '資料清單共' + str(len(pre_num)) + '個'
+        text = '資料清單共' + str(len(pre_num)) + '個，再等我一下......'
         print(text)
+        message4_label = tk.Label(work_frame, text=text)
+
+        message4_label.pack()
+        window.update()
+
 
         for checklist in pre_num:
             response = requests.post('https://www.sinya.com.tw/diy/show_option/', data={"prod_sub_slave_id": checklist})
@@ -80,26 +89,30 @@ def sinya():
                 j = (price.get('value').strip())
                 sinyaData[i] = j
 
-        message4_label.configure(text=str(text)+',欣亞數位資料撈取完畢!!!')
+        # message1_label.configure(text=str(text)+',欣亞數位資料撈取完畢!!!')
+        message5_label = tk.Label(work_frame, text='欣亞數位資料撈取完畢')
+        message5_label.pack()
         window.update()
         print('欣亞數位資料撈取完畢')
         sinya_search()
 
 
+
+
 def coolpc_search():
     coolpc_listbox.delete(0, 'end')
     keyword = keyword_entry.get().lower().split()
-    symbol = ['↪', '❤']
+    symbol = ['↪', '❤', '共有商品']
     for j in coolpcData:
         if all(x in j.lower() for x in keyword) and not any(x in j for x in symbol):
-            coolpc_listbox.insert(END, str(j)+' '+str(coolpcData[j][1:]))
+            coolpc_listbox.insert(END, str(j)+' '+str(coolpcData[j]))
 
 def sinya_search():
     sinya_listbox.delete(0, 'end')
     keyword = keyword_entry.get().lower().split()
     for k in sinyaData:
         if all(x in k.lower() for x in keyword):
-            sinya_listbox.insert(END, str(k) + ' ' + str(sinyaData[k][1:]))
+            sinya_listbox.insert(END, str(k) + ' ' + str(sinyaData[k]))
             # print(k, sinyaData[k])
 
 
@@ -109,7 +122,7 @@ header_label.pack()
 
 keyword_frame = tk.Frame(window)
 keyword_frame.pack(side='top')
-keyword_label = tk.Label(keyword_frame, text='請輸入要搜尋的商品')
+keyword_label = tk.Label(keyword_frame, text='請輸入要搜尋的商品，可以用空格多關鍵字搜尋')
 keyword_label.pack()
 
 keyword_entry = tk.Entry(keyword_frame, width='40')
@@ -155,18 +168,16 @@ work_frame = tk.Frame(window)
 work_frame.pack()
 work_label = tk.Label(work_frame, text='=========系統訊息==========')
 work_label.pack()
-message1_label = tk.Label(work_frame, text='')
-message1_label.pack()
-message2_label = tk.Label(work_frame, text='')
-message2_label.pack()
-message3_label = tk.Label(work_frame, text='')
-message3_label.pack()
-message4_label = tk.Label(work_frame, text='')
-message4_label.pack()
 
-get_btn = tk.Button(window, text='點我開撈',command=lambda:[coolpc(),sinya()])
+# message2_label = tk.Label(work_frame, text='')
+# message2_label.pack()
+# message3_label = tk.Label(work_frame, text='')
+# message3_label.pack()
+# message4_label = tk.Label(work_frame, text='')
+# message4_label.pack()
+
+get_btn = tk.Button(window, text='點我開撈', command=lambda: [coolpc(), sinya()])
 get_btn.pack()
-
 window.mainloop()
 
 
